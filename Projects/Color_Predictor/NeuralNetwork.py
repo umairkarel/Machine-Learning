778714cc6541f4dfa548f366bf0b02cd562d5a96
Umair Karel <umair.karel@gmail.com>
2021-01-11 20:52:33 +0530

    Updated

--- Projects/Color_Predictor/NeuralNetwork.py
+++ Projects/Color_Predictor/NeuralNetwork.py
@@ -4,68 +4,68 @@ import random
 
 # Classes
 class NeuralNetwork:
-	def __init__(self, numI, numH, numO):
-		self.input_nodes = numI
-		self.hidden_nodes = numH
-		self.output_nodes = numO
-		self.alpha = 0.1
+    def __init__(self, numI, numH, numO):
+        self.input_nodes = numI
+        self.hidden_nodes = numH
+        self.output_nodes = numO
+        self.alpha = 0.1
 
-		self.weigths_ih = np.random.uniform(-1, 1, (self.hidden_nodes, self.input_nodes))
-		self.weigths_ho = np.random.uniform(-1, 1, (self.output_nodes, self.hidden_nodes))
-		self.bias_h = np.random.uniform(-1, 1, (self.hidden_nodes, 1))
-		self.bias_o = np.random.uniform(-1, 1, (self.output_nodes, 1))
+        self.weigths_ih = np.random.uniform(-1, 1, (self.hidden_nodes, self.input_nodes))
+        self.weigths_ho = np.random.uniform(-1, 1, (self.output_nodes, self.hidden_nodes))
+        self.bias_h = np.random.uniform(-1, 1, (self.hidden_nodes, 1))
+        self.bias_o = np.random.uniform(-1, 1, (self.output_nodes, 1))
 
-	def sigmoid(self, x):
-		return 1/(1+np.exp(-x))
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
 
-	def drev_sigmoid(self, y):
-		return np.multiply(y,(1-y))
+    def drev_sigmoid(self, y):
+        return np.multiply(y,(1-y))
 
-	def feedforward(self, inputs):
-		inputs = np.array(inputs)
-		hidden = (self.weigths_ih @ inputs).reshape(-1,1)
-		hidden = np.add(hidden, self.bias_h)
-		hidden = self.sigmoid(hidden)
+    def feedforward(self, inputs):
+        inputs = np.array(inputs)
+        hidden = (self.weigths_ih @ inputs).reshape(-1,1)
+        hidden = np.add(hidden, self.bias_h)
+        hidden = self.sigmoid(hidden)
 
-		output = (self.weigths_ho @ hidden).reshape(-1,1)
-		output = np.add(output, self.bias_o)
-		output = self.sigmoid(output)
+        output = (self.weigths_ho @ hidden).reshape(-1,1)
+        output = np.add(output, self.bias_o)
+        output = self.sigmoid(output)
 
-		return output.flatten()
+        return output.flatten()
 
-	def train(self, inputs, targets):
-		inputs = np.array(inputs)
-		targets = np.array(targets)
-		# FeedForward
-		hidden = (self.weigths_ih @ inputs).reshape(-1,1)
-		hidden = np.add(hidden, self.bias_h)
-		hidden = self.sigmoid(hidden)
+    def train(self, inputs, targets):
+        inputs = np.array(inputs)
+        targets = np.array(targets)
+        # FeedForward
+        hidden = (self.weigths_ih @ inputs).reshape(-1,1)
+        hidden = np.add(hidden, self.bias_h)
+        hidden = self.sigmoid(hidden)
 
-		output = (self.weigths_ho @ hidden).reshape(-1,1)
-		output = np.add(output, self.bias_o)
-		output = self.sigmoid(output)
+        output = (self.weigths_ho @ hidden).reshape(-1,1)
+        output = np.add(output, self.bias_o)
+        output = self.sigmoid(output)
 
-		# Calculating Errors
-		targets = targets.reshape(-1,1)
-		output_error = np.subtract(targets, output)
-		hidden_errors = self.weigths_ho.T @ output_error 
-		
-		# BackPropagation
-		gradient = self.drev_sigmoid(output)
-		gradient = np.multiply(gradient, output_error)
-		gradient *= self.alpha
+        # Calculating Errors
+        targets = targets.reshape(-1,1)
+        output_error = np.subtract(targets, output)
+        hidden_errors = self.weigths_ho.T @ output_error 
 
-		weigths_ho_delta = gradient @ hidden.T
+        # BackPropagation
+        gradient = self.drev_sigmoid(output)
+        gradient = np.multiply(gradient, output_error)
+        gradient *= self.alpha
 
-		self.weigths_ho = np.add(self.weigths_ho, weigths_ho_delta)
-		self.bias_o = np.add(self.bias_o, gradient)
+        weigths_ho_delta = gradient @ hidden.T
 
-		hidden_grad = self.drev_sigmoid(hidden)
-		hidden_grad = np.multiply(hidden_grad, hidden_errors)
-		hidden_grad *= self.alpha
-		weigths_ih_delta = hidden_grad @ inputs.reshape(1,-1)  
+        self.weigths_ho = np.add(self.weigths_ho, weigths_ho_delta)
+        self.bias_o = np.add(self.bias_o, gradient)
 
-		self.weigths_ih = np.add(self.weigths_ih, weigths_ih_delta)
-		self.bias_h = np.add(self.bias_h, hidden_grad)
+        hidden_grad = self.drev_sigmoid(hidden)
+        hidden_grad = np.multiply(hidden_grad, hidden_errors)
+        hidden_grad *= self.alpha
+        weigths_ih_delta = hidden_grad @ inputs.reshape(1,-1)  
+
+        self.weigths_ih = np.add(self.weigths_ih, weigths_ih_delta)
+        self.bias_h = np.add(self.bias_h, hidden_grad)
 
 model = NeuralNetwork(3, 7, 1)
--- Projects/Color_Predictor/app.py
+++ Projects/Color_Predictor/app.py
@@ -18,49 +18,49 @@ FPS = 60
 # model = pickle.load(open('color_predictor.pickle', 'rb'))
 
 def map(n, start1, stop1, start2, stop2):
-	return ((n-start1)/(stop1-start1))*(stop2-start2)+start2
+    return ((n-start1)/(stop1-start1))*(stop2-start2)+start2
 
 def is_in_circle(x,y,pos):
-	sqy = (y - 190)**2
-	if pos:
-		sqx = (x - 125*3)**2
-	else:
-		sqx = (x - 125)**2
-	if math.sqrt(sqx+sqy) < 50:
-		return True
-	return False
+    sqy = (y - 190)**2
+    if pos:
+    	sqx = (x - 125*3)**2
+    else:
+    	sqx = (x - 125)**2
+    if math.sqrt(sqx+sqy) < 50:
+    	return True
+    return False
 
 def predict(r,g,b):
-	return round(model.feedforward([r,g,b])[0])
+    return round(model.feedforward([r,g,b])[0])
 
 def train(inputs, target):
-	model.train(inputs, target)
+    model.train(inputs, target)
 
 def draw(guess):
-	pygame.draw.line(screen, black, (250,0), (250,400), 3)
-	pygame.draw.circle(screen, white, (125, 190), 50)
-	pygame.draw.circle(screen, black, (125*3, 190), 50)
+    pygame.draw.line(screen, black, (250,0), (250,400), 3)
+    pygame.draw.circle(screen, white, (125, 190), 50)
+    pygame.draw.circle(screen, black, (125*3, 190), 50)
 
-	if guess == 0:
-		pygame.draw.circle(screen, white, (125, 100), 15)
-	elif guess == 1:
-		pygame.draw.circle(screen, black, (125*3, 100), 15)
+    if guess == 0:
+    	pygame.draw.circle(screen, white, (125, 100), 15)
+    elif guess == 1:
+    	pygame.draw.circle(screen, black, (125*3, 100), 15)
 
 def reset_window():
-	pygame.display.flip()
-	screen.fill(color)
-	clock.tick(FPS)
+    pygame.display.flip()
+    screen.fill(color)
+    clock.tick(FPS)
 
 def change_color():
-	global r,g,b
-	global guess
+    global r,g,b
+    global guess
 
-	r,g,b = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-	# print((r,g,b))
-	# print('------')
-	guess = predict(r,g,b)
-	# print(guess)
-	# print('----')
+    r,g,b = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    # print((r,g,b))
+    # print('------')
+    guess = predict(r,g,b)
+    # print(guess)
+    # print('----')
 	
 
 r,g,b = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
@@ -68,28 +68,28 @@ guess = predict(r, g, b)
 running = True
 
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			x,y = pygame.mouse.get_pos()
-			pos = 1 if x > 250 else 0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x,y = pygame.mouse.get_pos()
+            pos = 1 if x > 250 else 0
 
-			if is_in_circle(x,y,pos):
-				color = (r,g,b)
-				target = pos
+            if is_in_circle(x,y,pos):
+                color = (r,g,b)
+                target = pos
 
-				# print('###')
-				# print((r,g,b))
-				# print('###')
-				# print(guess)
-				train([r,g,b], target)
+                # print('###')
+                # print((r,g,b))
+                # print('###')
+                # print(guess)
+                train([r,g,b], target)
 
-				change_color()
+                change_color()
 			
-	color = (r,g,b)
-	draw(guess)
-	reset_window()
+    color = (r,g,b)
+    draw(guess)
+    reset_window()
 	
 with open('color_predictor.pickle', 'wb') as f:
 	pickle.dump(model, f)
